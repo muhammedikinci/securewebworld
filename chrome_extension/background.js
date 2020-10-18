0df37f5f5b73c550f11aa9d30c70cc55122b725d
muhammedikinci <muhammedikinci@outlook.com>
2020-10-18 15:04:59 +0300

    permission edited declarativeContent deleted

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -1,16 +1,3 @@
-chrome.runtime.onInstalled.addListener(function () {
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-        chrome.declarativeContent.onPageChanged.addRules([{
-            conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: {
-                    hostEquals: 'web.whatsapp.com'
-                },
-            })],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-        }]);
-    });
-});
-
 chrome.runtime.onConnect.addListener(function (port) {
     console.assert(port.name == "securewebwhatsapp");
     port.onMessage.addListener(function (msg) {
